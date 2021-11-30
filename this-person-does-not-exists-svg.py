b55c7cdc3f08eddf119cc6ad11d53b45e49b1f5e
Etienne Audouin <etienne.audouin-ext@pole-emploi.fr>
2021-11-30 12:32:22 +0100

    build(tpdne): simplification du script

--- this-person-does-not-exists-svg.py
+++ this-person-does-not-exists-svg.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-
+#!/usr/bin/python3
 """
 Use bash to download a file with this command
 
@@ -27,80 +25,53 @@ import time
 import random
 import argparse
 import requests
-import subprocess
 from pathlib import Path
 from multiprocessing import Pool
 from multiprocessing.dummy import Pool as ThreadPool
 
-from file_tools import render_svg
-
-parser = argparse.ArgumentParser(
-    description='Download images from thispersondoesnotexist.com')
-parser.add_argument('--threads', type=int, default=1,
-                    help='Number of threads to use')
-parser.add_argument('--output', type=str, default='images',
-                    help='Output directory')
-parser.add_argument('--start', type=int, default=1, help='Start index')
-parser.add_argument('--end', type=int, default=100, help='End index')
-parser.add_argument('--timeout', type=int, default=10,
-                    help='Timeout for each request')
-parser.add_argument('--retry', type=int, default=3,
-                    help='Number of retries for each request')
-parser.add_argument('--sleep', type=int, default=0,
-                    help='Sleep time between each request')
-parser.add_argument('--verbose', action='store_true', help='Verbose mode')
+parser = argparse.ArgumentParser(description='Download random images from thispersondoesnotexist.com')
+parser.add_argument('-n', '--number', type=int, default=100, help='number of images to download')
+parser.add_argument('-o', '--output', type=str, default='images', help='output directory')
+parser.add_argument('-s', '--sleep', type=int, default=0, help='sleep time between requests')
 args = parser.parse_args()
 
 
-def download(index):
-    url = 'https://thispersondoesnotexist.com/image'
-    headers = {
-        'authority': 'thispersondoesnotexist.com',
-        'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
-        'if-none-match': '"6161db77-6a883"',
-        'if-modified-since': 'Sat, 09 Oct 2021 18:12:07 GMT',
-        'sec-ch-ua-mobile': '?0',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
-        'sec-ch-ua-platform': '"Linux"',
-        'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'no-cors',
-        'sec-fetch-dest': 'image',
-        'referer': 'https://thispersondoesnotexist.com/',
-        'accept-language': 'fr-FR,fr;q=0.9,de-DE;q=0.8,de;q=0.7,en-US;q=0.6,en;q=0.5,eo;q=0.4',
-    }
-    for i in range(args.retry):
-        try:
-            response = requests.get(url, headers=headers, timeout=args.timeout)
-            if response.status_code == 200:
-                with open(os.path.join(args.output, f'{index}.jpg'), 'wb') as f:
-                    f.write(response.content)
-                if args.verbose:
-                    print(f'[{index}] Downloaded')
-                return
-        except Exception as e:
-            if args.verbose:
-                print(f'[{index}] Error: {e}')
-            time.sleep(args.sleep)
-    if args.verbose:
-        print(f'[{index}] Failed')
+def download_image(url):
+    """
+    Download an image from thispersondoesnotexist.com
+    """
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        return None
 
 
-def convert(index):
+def main():
     """
-    Foreach downloaded pictures, make an svg using potrace
+    Main function
     """
-    render_svg(f'./images/{index}.jpg')
+    # Create output directory
+    output_dir = Path(args.output)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Create a pool of threads
+    pool = ThreadPool(4)
+
+    # Download images
+    for i in range(args.number):
+        url = 'https://thispersondoesnotexist.com/image'
+        image = download_image(url)
+        if image is not None:
+            # Save image
+            filename = '{}.jpg'.format(i)
+            with open(output_dir / filename, 'wb') as f:
+                f.write(image)
+            print('[+] {}'.format(filename))
+        else:
+            print('[-] {}'.format(filename))
+        time.sleep(args.sleep)
 
 
 if __name__ == '__main__':
-    if not os.path.exists(args.output):
-        os.makedirs(args.output)
-    pool = ThreadPool(args.threads)
-    pool.map(download, range(args.start, args.end + 1))
-    pool.close()
-    pool.join()
-    pool = ThreadPool(args.threads)
-    pool.map(convert, range(args.start, args.end + 1))
-    pool.close()
-    pool.join()
+    main()
